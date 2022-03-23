9273ae4cb6c71d66d0f49b51023ca4107b6d25f2
quincinia <eanj13@gmail.com>
2022-03-23 11:58:00 -0700

    Added server location

--- server/main.go
+++ server/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
 	// https://pkg.go.dev/net/http#example-FileServer
+	fmt.Println("Serving on http://localhost:8080/")
 	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("./recipes"))))
 }
