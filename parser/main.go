a8afe296ede93f464ce2e399c63c719b9712dba4
quincinia <eanj13@gmail.com>
2022-03-25 12:02:53 -0700

    Parser now finds and prints ingredients list

--- parser/main.go
+++ parser/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -28,20 +29,43 @@ func main() {
 	if err != nil {
 		log.Fatalln("Error:", err)
 	}
-	findIngredientList(doc)
+
+	list, err := findIngredientList(doc)
+	if err != nil {
+		log.Fatalln("Error:", err)
+	}
+	printIngredientList(list)
 }
 
 // See https://pkg.go.dev/golang.org/x/net/html#example-Parse
-func findIngredientList(node *html.Node) {
+func findIngredientList(node *html.Node) (*html.Node, error) {
 	if node.Type == html.ElementNode && node.Data == "ul" {
 		for _, a := range node.Attr {
-			if a.Key == "class" {
-				fmt.Println(a.Val)
-				break
+			if a.Key == "class" && a.Val == "wprm-recipe-ingredients" {
+				return node, nil
 			}
 		}
 	}
 	for c := node.FirstChild; c != nil; c = c.NextSibling {
-		findIngredientList(c)
+		list, _ := findIngredientList(c)
+		if list != nil {
+			return list, nil
+		}
+	}
+	return nil, errors.New("ingredient list does not exist")
+}
+
+func printIngredientList(list *html.Node) {
+	for li := list.FirstChild; li != nil; li = li.NextSibling {
+		for child := li.FirstChild; child != nil; child = child.NextSibling {
+			if child.Type == html.ElementNode && child.Data == "span" {
+				for _, a := range child.Attr {
+					if a.Val == "wprm-recipe-ingredient-name" {
+						// The first child should be a text node
+						fmt.Println(child.FirstChild.Data)
+					}
+				}
+			}
+		}
 	}
 }
