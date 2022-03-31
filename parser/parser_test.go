17f0d4e3e0dcec5f33f7c3a7dd3947dfb250b5a8
Jacob Gayban <eanj13@gmail.com>
2022-03-31 14:32:32 -0700

    Added parseFromFile; reformatted tests

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,46 +1,66 @@
 package parser
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"golang.org/x/net/html"
+)
 
 // assert function
 
 // setup function
 // read data from static file instead of making network calls
+func parseFromFile(filename string) (*html.Node, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	return html.Parse(file)
+}
+
 // grab recipe card and use that for all tests
 
 func TestSimple(t *testing.T) {
-	t.Run("slow-cooker-mashed-potatoes", func(t *testing.T) {
+	tests := []string{
+		"slow-cooker-mashed-potatoes",
+		"olive-oil-mashed-potatoes",
+		"fluffy-garlic-herb-mashed-potatoes",
+	}
 
-	})
+	for _, test := range tests {
 
-	t.Run("olive-oil-mashed-potatoes", func(t *testing.T) {
+		t.Run(test, func(t *testing.T) {
 
-	})
-
-	t.Run("fluffy-garlic-herb-mashed-potatoes", func(t *testing.T) {
-
-	})
+		})
+	}
 }
 
 // Ingredients may partially (need example) or fully be contained within a link
 func TestIngredientContainsLink(t *testing.T) {
-	// Fully contained
-	t.Run("beef-cabbage-stir-fry", func(t *testing.T) {
+	tests := []string{
+		"beef-cabbage-stir-fry", // fully contained
+		"beef-taco-pasta",       // fully contained
+	}
 
-	})
+	for _, test := range tests {
 
-	// Fully contained
-	t.Run("beef-taco-pasta", func(t *testing.T) {
+		t.Run(test, func(t *testing.T) {
 
-	})
+		})
+	}
 }
 
 func TestMultipleIngredientLists(t *testing.T) {
-	t.Run("beef-cabbage-stir-fry", func(t *testing.T) {
+	tests := []string{
+		"beef-cabbage-stir-fry",
+		"chili-roasted-potatoes",
+	}
 
-	})
+	for _, test := range tests {
 
-	t.Run("chili-roasted-potatoes", func(t *testing.T) {
-		
-	})
+		t.Run(test, func(t *testing.T) {
+
+		})
+	}
 }
