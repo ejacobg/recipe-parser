739a7a36eeb0ca84386325b17c9f7de59fec5c99
ejacobg <ejacobg@outlook.com>
2022-10-11 15:09:58 -0700

    Refactored to use matcher functions

--- parser/parser.go
+++ parser/parser.go
@@ -10,108 +10,103 @@ import (
 )
 
 // See https://pkg.go.dev/golang.org/x/net/html#example-Parse
-func FindIngredientList(node *html.Node) (*html.Node, error) {
-	if node.Type == html.ElementNode && node.Data == "ul" {
-		for _, a := range node.Attr {
-			if a.Key == "class" && a.Val == "wprm-recipe-ingredients" {
-				return node, nil
-			}
-		}
-	}
-	for c := node.FirstChild; c != nil; c = c.NextSibling {
-		list, _ := FindIngredientList(c)
-		if list != nil {
-			return list, nil
-		}
-	}
-	return nil, errors.New("ingredient list does not exist")
-}
 
-func PrintIngredientList(list *html.Node) {
-	for li := list.FirstChild; li != nil; li = li.NextSibling {
-		for child := li.FirstChild; child != nil; child = child.NextSibling {
-			if child.Type == html.ElementNode && child.Data == "span" {
-				for _, a := range child.Attr {
-					if a.Val == "wprm-recipe-ingredient-name" {
-						// The first child should be a text node
-						fmt.Println(child.FirstChild.Data)
-					}
+// FindRecipeCard returns the node representing the enclosing <div> of the recipe.
+// All the relevant recipe information is rooted at this node.
+func FindRecipeCard(node *html.Node) *html.Node {
+	matcher := func(node *html.Node) bool {
+		if node.Type == html.ElementNode && node.DataAtom == atom.Div {
+			for _, a := range node.Attr {
+				if a.Key == "class" && a.Val == "wprm-recipe-container" {
+					return true
 				}
 			}
 		}
+		return false
 	}
+	return FindNode(node, matcher)
 }
 
-// Technically don't need to return an error because we can just check for nil
-func FindRecipeCard(node *html.Node) *html.Node {
-	if node.Type == html.ElementNode && node.Data == "div" {
-		for _, a := range node.Attr {
-			if a.Key == "class" && a.Val == "wprm-recipe-container" {
-				return node
+// FindIngredientLists returns all nodes representing a list of ingredients.
+// Some recipes group their ingredients under different headers (eg. "Sauce", "Garnishes"),
+// each with their own list.
+func FindIngredientLists(node *html.Node) []*html.Node {
+	matcher := func(node *html.Node) (keep, exit bool) {
+		if node.Type == html.ElementNode && node.DataAtom == atom.Ul {
+			for _, a := range node.Attr {
+				if a.Key == "class" && a.Val == "wprm-recipe-ingredients" {
+					keep = true
+					exit = true // No nested ingredients lists
+				}
 			}
 		}
+		return
 	}
-	for c := node.FirstChild; c != nil; c = c.NextSibling {
-		card := FindRecipeCard(c)
-		if card != nil {
-			return card
-		}
-	}
-	return nil
+	return FindNodes(node, matcher)
 }
 
+// FindInstructionsList returns the node representing the list of recipe instructions.
+// This implementation currently assumes that there is only 1 master list of instructions.
 func FindInstructionsList(node *html.Node) (*html.Node, error) {
-	if node.Type == html.ElementNode && node.Data == "ul" {
-		for _, a := range node.Attr {
-			if a.Key == "class" && a.Val == "wprm-recipe-instructions" {
-				return node, nil
+	matcher := func(node *html.Node) bool {
+		if node.Type == html.ElementNode && node.DataAtom == atom.Ul {
+			for _, a := range node.Attr {
+				if a.Key == "class" && a.Val == "wprm-recipe-instructions" {
+					return true
+				}
 			}
 		}
+		return false
 	}
-	for c := node.FirstChild; c != nil; c = c.NextSibling {
-		list, _ := FindInstructionsList(c)
-		if list != nil {
-			return list, nil
-		}
+	if il := FindNode(node, matcher); il != nil {
+		return il, nil
 	}
 	return nil, errors.New("instructions list does not exist")
 }
 
-// Returns the first element underneath and including `node` that has the given class value (as given in the HTML)
-func GetElementWithClass(node *html.Node, tagname, class string) *html.Node {
-	if node.Type == html.ElementNode && node.Data == tagname {
-		for _, a := range node.Attr {
-			if a.Key == "class" && a.Val == class {
-				return node
+// GetElementWithClass returns the first element underneath and including `node` that has the given
+// class value (as given in the HTML).
+func GetElementWithClass(node *html.Node, tagname atom.Atom, class string) *html.Node {
+	matcher := func(node *html.Node) bool {
+		if node.Type == html.ElementNode && node.DataAtom == tagname {
+			for _, a := range node.Attr {
+				if a.Key == "class" && a.Val == class {
+					return true
+				}
 			}
 		}
+		return false
 	}
-	for c := node.FirstChild; c != nil; c = c.NextSibling {
-		element := GetElementWithClass(c, tagname, class)
-		if element != nil {
-			return element
-		}
-	}
-	return nil
+	return FindNode(node, matcher)
 }
 
-// Returns the first text node under the given node
+// GetTextNode returns the first text node under the given node.
 func GetTextNode(node *html.Node) *html.Node {
-	if node.Type == html.TextNode {
+	matcher := func(node *html.Node) bool {
+		return node.Type == html.TextNode
+	}
+	return FindNode(node, matcher)
+}
+
+// FindNode will return the first node that the matcher function accepts.
+func FindNode(node *html.Node, matcher func(node *html.Node) bool) *html.Node {
+	if matcher(node) {
 		return node
 	}
 	for c := node.FirstChild; c != nil; c = c.NextSibling {
-		textNode := GetTextNode(c)
-		if textNode != nil {
-			return textNode
+		n := FindNode(c, matcher)
+		if n != nil {
+			return n
 		}
 	}
 	return nil
 }
 
-// TraverseNode collecting the nodes that match the given function
-// See https://gist.github.com/Xeoncross/8bbb84bc4bf540bd907f79ee17c4e1fc
-func TraverseNode(doc *html.Node, matcher func(node *html.Node) (bool, bool)) (nodes []*html.Node) {
+// FindNodes will gather all nodes that the matcher function accepts.
+// The matcher function's second return value indicates whether to search the children of the
+// current node.
+// See TraverseNode from https://gist.github.com/Xeoncross/8bbb84bc4bf540bd907f79ee17c4e1fc
+func FindNodes(node *html.Node, matcher func(node *html.Node) (bool, bool)) (nodes []*html.Node) {
 	var keep, exit bool
 	var f func(*html.Node)
 	f = func(n *html.Node) {
@@ -126,23 +121,8 @@ func TraverseNode(doc *html.Node, matcher func(node *html.Node) (bool, bool)) (n
 			f(c)
 		}
 	}
-	f(doc)
+	f(node)
 	return nodes
-}
-
-func FindIngredientLists(node *html.Node) []*html.Node {
-	matcher := func(node *html.Node) (keep, exit bool) {
-		if node.Type == html.ElementNode && node.DataAtom == atom.Ul {
-			for _, a := range node.Attr {
-				if a.Key == "class" && a.Val == "wprm-recipe-ingredients" {
-					keep = true
-					exit = true // No nested ingredients lists
-				}
-			}
-		}
-		return
-	}
-	return TraverseNode(node, matcher)
 }
 
 func PrintNode(node *html.Node) {
