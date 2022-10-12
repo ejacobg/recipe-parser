535f86df11970463aaf37927eae51a2ee4209475
ejacobg <ejacobg@outlook.com>
2022-10-12 16:45:40 -0700

    Testing mongodb integration

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
@@ -8,6 +9,10 @@ import (
 	"os"
 
 	"github.com/ejacobg/recipe-parser/recipe"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"golang.org/x/net/html"
 )
@@ -32,6 +37,8 @@ func main() {
 	log.SetFlags(0)
 	flag.Parse()
 	args := flag.Args()
+
+	mongodb()
 
 	if len(args) < 1 {
 		log.Fatalln("Error: too few arguments")
@@ -71,4 +78,45 @@ func main() {
 	}
 
 	fmt.Println("Recipe saved to " + *dbPath + args[0])
+}
+
+func mongodb() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+		return
+	}
+
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Println("MONGODB_URI variable must be set")
+	}
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	coll := client.Database("mongo_dev").Collection("recipes")
+	r, err := recipe.FromJSON("./database/slow-cooker-mashed-potatoes.json")
+	if err != nil {
+		log.Println("Could not read recipe from file")
+	}
+
+	filter := bson.D{{"id", r.ID}}
+	opts := options.Replace().SetUpsert(true)
+	result, err := coll.ReplaceOne(context.TODO(), filter, r, opts)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(
+		"Matched:", result.MatchedCount, "Modified:", result.MatchedCount, "Upserted:",
+		result.UpsertedCount,
+	)
+	return
 }
